65733bce9df18aef11627456a3dd3f1058f5c366
Mylene <pont.mylene@gmail.com>
2021-12-29 11:26:22 +0900

    function to create array of number and rolldice to get new array done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import Die from "./Die"
 
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  // GET RAND NUMBERS ON THE DICE
+  function allNewDice() {
+    // create empty array
+    const newDice = []
+    // loop and push numbers into array
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6)) 
+        // Math.ceil= start arr at 1 (.floor starts at 0)
+    }
+    return newDice
+    }
+
+    function rollDice() {
+      setDice(allNewDice())
+    }
+
+    // MAP OVER DICE STATE TO GET NEW NUMBERS RENDERED
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="3" />
-                <Die value="4" />
-                <Die value="5" />
-                <Die value="6" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
+                {diceElements}
             </div>
+            <button onClick={rollDice}>Roll</button>
         </main>
     )
 }
